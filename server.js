bc46b5cf603c34de3526ee67ce28c1a06024495d
Pablo Goncalves <pablo.goncalves@discover.com.br>
2018-12-19 10:31:30 -0200

    Database connection, methods updated and some new things.

--- server.js
+++ server.js
@@ -2,71 +2,96 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'yourDBpassword',
+        database: 'yourDBname'
+    }
+});
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'Pablo',
-            email: 'pablo@gmail.com',
-            password: 'oi123',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-};
-
-const verifyUserId = (id) => {
-    let userFound = database.users.find(user => {
-        return user.id === id;
-    });
-    return userFound;
-}
-
-app.get('/', (req, res) => {
-    res.send(database.users);
-});
+app.get('/', (req, res) => {});
 
 app.post('/signin', (req, res) => {
-
+    const { email, password } = req.body;
+    db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => res.status(400).json("Unable to get user"));
+            } else
+                res.status(400).json("Wrong credentials");
+        })
+        .catch(err => res.status(400).json("Wrong credentials"));
 });
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '',
-        name: name,
-        email: email,
-        password: password,
-        entries: 0,
-        joined: new Date()
-    });
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+        })
+            .then(trx.commit)
+            .catch(trx.rollback);
+    }).catch(err => res.status(400).json("Unable to register"));
 });
 
 app.get('/profile/:id', (req, res) => {
-    const user = verifyUserId(req.params.id);
-    if (user) {
-        res.json(user);
-    } else {
-        res.status(404).json("None user has been found!");
-    }
+    const { id } = req.params;
+    db.select('*').from('users').where({id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            } else
+                throw e;
+        })
+        .catch(err => {
+            res.status(404).json("None user has been found!");
+        });
 });
 
 app.put('/image', (req, res) => {
-    const user = verifyUserId(req.body.id);
-    if (user) {
-        user.entries++
-        res.json(user.entries);
-    } else {
-        res.status(404).json("None user has been found!");
-    }
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get entries'));
 });
 
 app.listen(3000, () => {
-    console.log("app is running on port 3000");
+    console.log("App is running on port 3000");
 });
